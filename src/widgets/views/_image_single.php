a13c97f091a75fa506de610684a5247df159f510
Vital Ozierski <v.ozierski@blak-it.com>
2019-06-27 15:29:10 +0300

    1.2.9 - add ru/en localizations

--- src/widgets/views/_image_single.php
+++ src/widgets/views/_image_single.php
@@ -10,10 +10,10 @@
           d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"></path>
     </svg>
     <div class="widget-image__text">
-      <strong>Выберите файл</strong> или перетащите его сюда
+      <strong><?= Yii::t('filestorage', 'Select a file') ?></strong> <?= Yii::t('filestorage', 'or drop it here') ?>
     </div>
     <div class="widget-image__loading" style="display: none">
-      Загрузка...
+        <?= Yii::t('filestorage', 'Loading...') ?>
     </div>
   </label>
 </div>
@@ -22,6 +22,6 @@
     <img src="<?= $model ? $model->getUrl() : null ?>">
   </div>
   <div class="widget-image__model-actions">
-    <button class="js-widget-image-delete">Удалить</button>
+    <button class="js-widget-image-delete"><?= Yii::t('filestorage', 'Remove') ?></button>
   </div>
 </div>
